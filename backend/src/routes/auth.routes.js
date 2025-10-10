9e277ed97b300a3e2d9687cbc7f832e1d0fe192a
Pablo Vergara <pablo.j98@gmail.com>
2025-10-10 18:16:22 -0400

    feat(docs): Agregar documentacion a /api/v1/auth/register-adviser

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,10 +6,119 @@ const router = Router();
 
 router.post('/register', validateRegister, authController);
 
-// 201 -> creasion de cta exitosa
-// 400 -> no viene ningun dato
-// 422 -> falta algun dato
-// 500 -> error interno
+/**
+ * @swagger
+ * /auth/register-adviser:
+ *   post:
+ *     tags:
+ *      - "Autenticación" 
+ *     summary: Crea una cuenta de asesor.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Registro-asesor'
+ *     responses:
+ *       201:
+ *         description: Creacion de cuenta asesor exitosa.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     nombres:
+ *                       type: string
+ *                       description: Nombres del usuario.
+ *                       example: "Jhon Richard"
+ *                     appellidos:
+ *                       type: string
+ *                       description: Apellidos del usuario.
+ *                       example: "Doe Smith"
+ *                     email:
+ *                       type: string
+ *                       description: El email del usuario.
+ *                       example: "jhondoe05@gmail.com"
+ *                     token:
+ *                       type: string
+ *                       description: JSON web Token.
+ *                       example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
+ *                     cargo:
+ *                       type: string
+ *                       description: Cargo dentro de la empresa.
+ *                       example: "asesor"
+ *       400:
+ *         description: Ningun dato fue recibido.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     status:
+ *                       type: string
+ *                       description: Estado de la respuesta.
+ *                       example: "error"
+ *                     message:
+ *                       type: string
+ *                       description: Mensaje de la respuesta.
+ *                       example: "email, nombres, apellidos y password son obligatorios."
+ *       422:
+ *         description: Algun dato requerido no fue enviado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     status:
+ *                       type: string
+ *                       description: Estado de la respuesta.
+ *                       example: "error"
+ *                     message:
+ *                       type: string
+ *                       description: Mensaje de la respuesta.
+ *                       example: "Datos de registro inválidos."
+ *                     errors:
+ *                       type: array 
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           field:
+ *                             type: string
+ *                             description: Campo donde se genero el error.
+ *                             example: "password"
+ *                           message:
+ *                             type: string
+ *                             description: Mensaje específico para ese error.
+ *                             example: '\"password\" is required'
+ *       500:
+ *         description: Error interno del servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     status:
+ *                       type: string
+ *                       description: Estado de la respuesta.
+ *                       example: "error"
+ *                     message:
+ *                       type: string
+ *                       description: Mensaje de la respuesta.
+ *                       example: "Error interno del servidor."
+ * 
+*/
 router.post('/register-adviser', validateRegisterAdviser, authRegisterAdviserController);
 
 /**
@@ -55,7 +164,7 @@ router.post('/register-adviser', validateRegisterAdviser, authRegisterAdviserCon
  *                     cargo:
  *                       type: string
  *                       description: Cargo dentro de la empresa.
- *                       example: "Supervisor"
+ *                       example: "user"
  *       400:
  *         description: Ningun dato fue recibido.
  *         content:
@@ -122,7 +231,25 @@ router.post('/register-adviser', validateRegisterAdviser, authRegisterAdviserCon
  *                           message:
  *                             type: string
  *                             description: Mensaje específico para ese error.
- *                             example: '\"password\" is required' 
+ *                             example: '\"password\" is required'
+ *       500:
+ *         description: Error interno del servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     status:
+ *                       type: string
+ *                       description: Estado de la respuesta.
+ *                       example: "error"
+ *                     message:
+ *                       type: string
+ *                       description: Mensaje de la respuesta.
+ *                       example: "Error interno del servidor." 
  * 
 */
 router.post('/login', validationLogin, authLoginController)
