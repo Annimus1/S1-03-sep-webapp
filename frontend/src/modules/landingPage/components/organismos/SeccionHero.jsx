dadf51759b6b81d5a9c7354911401f848bfdc3d3
Pablo Vergara <pablo.j98@gmail.com>
2025-10-30 09:09:45 -0400

    fix(imagenes): Crear Imagenes para Landing

--- frontend/src/modules/landingPage/components/organismos/SeccionHero.jsx
+++ frontend/src/modules/landingPage/components/organismos/SeccionHero.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { TarjetaCaracteristica } from '../moleculas/TarjetaCaracteristica';
+import PrimeraImagen from '../../../../assets/Silver.png'
+import SegundaImagen from '../../../../assets/MacBookPro.png'
 
 /**
  * Sección principal con título y tarjetas de características
  * Es la primera sección que ve el usuario
  */
 export const SeccionHero = () => (
-  <section style={{padding: '50px 0' }}>
+  <section style={{ padding: '50px 0' }}>
     <div className="container">
       {/* Título principal y subtítulo */}
       <div className="text-center text-white mb-4">
@@ -17,20 +19,23 @@ export const SeccionHero = () => (
           Solicita, carga tus documentos y haz seguimiento en <span style={{ color: '#7B1FA2', fontWeight: 'bold' }}>tiempo real</span>, todo desde una sola plataforma.
         </p>
       </div>
-      
+
       {/* Tarjetas de características */}
-      <div className="row g-3" style={{ maxWidth: '1200px', margin: '0 auto'  }}>
+      <div className="row g-3" style={{ maxWidth: '1200px', margin: '0 auto' }}>
 
         <div className="col-12 col-md-6">
-          <TarjetaCaracteristica altura="500px">
+          <TarjetaCaracteristica altura="500px" src={PrimeraImagen} alt="Primera Img">
             Tu crédito PyME <span style={{ color: '#7B1FA2', fontWeight: 'bold' }}>100%</span> digital, rápido y seguro.
           </TarjetaCaracteristica>
         </div>
 
         <div className="col-12 col-md-6">
-          <TarjetaCaracteristica titulo="" 
-          textoBoton="Impulsa tu negocio hoy"
-          altura="400px"/>
+          <TarjetaCaracteristica titulo=""
+            textoBoton="Impulsa tu negocio hoy"
+            altura="400px"
+            src={SegundaImagen}
+            paddinY='60px'
+            alt="Segunda IMG" />
         </div>
       </div>
     </div>
